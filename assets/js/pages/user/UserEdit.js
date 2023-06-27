eee01a7c830500df5ba251b75aeac34f98b9fb63
Fullfifax <fullfifax1245@gmail.com>
2023-06-27 17:01:26 +0000

    remove div.container to user pages

--- assets/js/pages/user/UserEdit.js
+++ assets/js/pages/user/UserEdit.js
@@ -68,76 +68,74 @@ function UserEdit() {
   
     return (
         <ContainerLayout>
-            <div className="container">
-                <h2 className="text-center mt-5 mb-3 custom-font-amcap text-orange">Edit User</h2>
-                <div className="card">
-                    <div className="card-header">
-                        <Link 
-                            className="btn btn-darkblue float-end"
-                            to="/user">View All Users
-                        </Link>
+            <h2 className="text-center mt-5 mb-3 custom-font-amcap text-orange">Edit User</h2>
+            <div className="card">
+                <div className="card-header">
+                    <Link 
+                        className="btn btn-darkblue float-end"
+                        to="/user">View All Users
+                    </Link>
+                </div>
+                <div className="card-body">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input 
+                            onChange={(event)=>{setName(event.target.value)}}
+                            value={name || ''}
+                            type="text"
+                            className="form-control"
+                            id="name"
+                            name="name"/>
                     </div>
-                    <div className="card-body">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input 
-                                onChange={(event)=>{setName(event.target.value)}}
-                                value={name || ''}
-                                type="text"
-                                className="form-control"
-                                id="name"
-                                name="name"/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="firstname">Firstname</label>
-                            <input 
-                                onChange={(event)=>{setFirstname(event.target.value)}}
-                                value={firstname || ''}
-                                type="text"
-                                className="form-control"
-                                id="firstname"
-                                name="firstname"/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="cin">CIN</label>
-                            <input 
-                                onChange={(event)=>{setCin(event.target.value)}}
-                                value={cin || ''}
-                                type="text"
-                                className="form-control"
-                                id="cin"
-                                name="cin"/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="age">Age</label>
-                            <input 
-                                onChange={(event)=>{setAge(event.target.value)}}
-                                value={age || ''}
-                                type="text"
-                                className="form-control"
-                                id="age"
-                                name="age"/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="address">Address</label>
-                            <input 
-                                onChange={(event)=>{setAddress(event.target.value)}}
-                                value={address || ''}
-                                type="text"
-                                className="form-control"
-                                id="address"
-                                name="address"/>
-                        </div>
-                        <button 
-                            disabled={isSaving}
-                            onClick={handleSave} 
-                            type="button"
-                            className="btn btn-orange w-100 mt-3">
-                            Save User
-                        </button>
-                    </form>
+                    <div className="form-group">
+                        <label htmlFor="firstname">Firstname</label>
+                        <input 
+                            onChange={(event)=>{setFirstname(event.target.value)}}
+                            value={firstname || ''}
+                            type="text"
+                            className="form-control"
+                            id="firstname"
+                            name="firstname"/>
                     </div>
+                    <div className="form-group">
+                        <label htmlFor="cin">CIN</label>
+                        <input 
+                            onChange={(event)=>{setCin(event.target.value)}}
+                            value={cin || ''}
+                            type="text"
+                            className="form-control"
+                            id="cin"
+                            name="cin"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="age">Age</label>
+                        <input 
+                            onChange={(event)=>{setAge(event.target.value)}}
+                            value={age || ''}
+                            type="text"
+                            className="form-control"
+                            id="age"
+                            name="age"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="address">Address</label>
+                        <input 
+                            onChange={(event)=>{setAddress(event.target.value)}}
+                            value={address || ''}
+                            type="text"
+                            className="form-control"
+                            id="address"
+                            name="address"/>
+                    </div>
+                    <button 
+                        disabled={isSaving}
+                        onClick={handleSave} 
+                        type="button"
+                        className="btn btn-orange w-100 mt-3">
+                        Save User
+                    </button>
+                </form>
                 </div>
             </div>
         </ContainerLayout>
--- assets/js/pages/user/UserList.js
+++ assets/js/pages/user/UserList.js
@@ -62,65 +62,63 @@ function UserList() {
   
     return (
         <ContainerLayout>
-           <div className="container">
             <h2 className="text-center mt-5 mb-3 custom-font-amcap text-orange">Users List</h2>
-                <div className="card">
-                    <div className="card-header">
-                        <Link 
-                            to="/">
-                                <img src={backIcon} width="65px" height="65px" alt='back' />
-                        </Link>
-                        <Link 
-                            className="float-end"
-                            to="/user/create">
-                                <img src={addIcon} width="65px" height="65px" alt='add' />
-                        </Link>
-                    </div>
-                    <div className="card-body">
-              
-                        <table className="table table-bordered">
-                            <thead>
-                                <tr className='text-orange'>
-                                    <th>Name</th>
-                                    <th>Description</th>
-                                    <th>Age</th>
-                                    <th>CIN</th>
-                                    <th>Address</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {userList.map((user, key)=>{
-                                    return (
-                                        <tr key={key}>
-                                            <td>{user.name}</td>
-                                            <td>{user.firstname}</td>
-                                            <td>{user.age}</td>
-                                            <td>{user.cin}</td>
-                                            <td>{user.address}</td>
-                                            <td className='d-flex justify-content-center'>
-                                                <Link
-                                                    to={`/user/show/${user.id}`}
-                                                    className="btn btn-outline-info mx-1">
-                                                    <img className="custom-icon" src={showIcon} alt="show"/>
-                                                </Link>
-                                                <Link
-                                                    className="btn btn-outline-success mx-1"
-                                                    to={`/user/edit/${user.id}`}>
-                                                    <img className="custom-icon" src={editIcon} alt="edit"/>
-                                                </Link>
-                                                <button 
-                                                    onClick={()=>handleDelete(user.id)}
-                                                    className="btn btn-outline-danger mx-1">
-                                                    <img className="custom-icon" src={deleteIcon} alt="delete"/>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
+            <div className="card">
+                <div className="card-header">
+                    <Link 
+                        to="/">
+                            <img src={backIcon} width="65px" height="65px" alt='back' />
+                    </Link>
+                    <Link 
+                        className="float-end"
+                        to="/user/create">
+                            <img src={addIcon} width="65px" height="65px" alt='add' />
+                    </Link>
+                </div>
+                <div className="card-body">
+            
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr className='text-orange'>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Age</th>
+                                <th>CIN</th>
+                                <th>Address</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userList.map((user, key)=>{
+                                return (
+                                    <tr key={key}>
+                                        <td>{user.name}</td>
+                                        <td>{user.firstname}</td>
+                                        <td>{user.age}</td>
+                                        <td>{user.cin}</td>
+                                        <td>{user.address}</td>
+                                        <td className='d-flex justify-content-center'>
+                                            <Link
+                                                to={`/user/show/${user.id}`}
+                                                className="btn btn-outline-info mx-1">
+                                                <img className="custom-icon" src={showIcon} alt="show"/>
+                                            </Link>
+                                            <Link
+                                                className="btn btn-outline-success mx-1"
+                                                to={`/user/edit/${user.id}`}>
+                                                <img className="custom-icon" src={editIcon} alt="edit"/>
+                                            </Link>
+                                            <button 
+                                                onClick={()=>handleDelete(user.id)}
+                                                className="btn btn-outline-danger mx-1">
+                                                <img className="custom-icon" src={deleteIcon} alt="delete"/>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </ContainerLayout>
