5814c4d5417ee10371f2ac639f0c1d6a894a8e97
Fullfifax <fullfifax1245@gmail.com>
2023-07-14 17:28:41 +0000

    add migrate code snippet

--- assets/js/components/codeSnippets/basicCrud/BasicCrud.js
+++ assets/js/components/codeSnippets/basicCrud/BasicCrud.js
@@ -3,14 +3,18 @@ import CodeBlock from '../../codeBlock/CodeBlock';
 
 const BasicCrud = () => {
     const codeSnippetEntityCommandCreation = `php bin/console make:entity`;
-    const codeSnippetMigration = `php bin/console make:migration`;
+    const codeSnippetMakeMigration = `php bin/console make:migration`;
+    const codeSnippetMigrate = `php bin/console doctrine:migrations:migrate`;
 
     return <>
         <p>Create an user entity and put name, firstname, age, cin, adress as properties</p>
         <CodeBlock code={codeSnippetEntityCommandCreation} />
         <br/>
         <p>Once you are done with user entity creation, make migration</p>
-        <CodeBlock code={codeSnippetMigration} />
+        <CodeBlock code={codeSnippetMakeMigration} />
+        <br/>
+        <p>After a migration file created (which contains SQL). We will then run the SQL using this command</p>
+        <CodeBlock code={codeSnippetMigrate} />
     </>
 };
 
